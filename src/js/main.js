69ef69091126976986b1e1742775d83bc9d0a2a3
David Rogers <al.the.x@gmail.com>
2015-10-14 12:57:03 -0400

    Just replace the mustaches that _already exist_ first.

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,11 @@
     .run([ '$rootScope', function($scope){
       // PUT YOUR CODE IN HERE, PLEASE
 
+      $scope.listing = {
+        title: 'Something Random!',
+        price: 1234.56,
+      };
+
       // AND DON'T DELETE ANYTHING BELOW
     } ])
   ; // END module(tiy-catalog--trending)
