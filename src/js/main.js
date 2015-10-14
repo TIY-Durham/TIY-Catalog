8fb27eadf7c06cc3bbed79d649c367832e00faf7
David Rogers <al.the.x@gmail.com>
2015-10-14 12:56:08 -0400

    Give ourselves a clearer starting point

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,10 @@
 ;(function(){ // No params necessary...
   // TODO: Create a module named "tiy-catalog--trending"
-  angular.module('tiy-catalog--trending', [ ]);
+  angular.module('tiy-catalog--trending', [ ])
+    .run([ '$rootScope', function($scope){
+      // PUT YOUR CODE IN HERE, PLEASE
+
+      // AND DON'T DELETE ANYTHING BELOW
+    } ])
+  ; // END module(tiy-catalog--trending)
 })(); // "Simple" IIFE
