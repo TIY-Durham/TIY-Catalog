0618a7768307387d7ea225dfa56c593e88716949
David <dmwhite@gmail.com>
2015-10-14 20:49:39 -0400

    updated main.js to actually control the panels

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,9 @@
 //controller for the item-tabs area of the Listing/Product page
 $("nav#item-tabs a").click(function(){
   $(this).addClass("active").siblings().removeClass("active");
+  var text = $(this).attr("href");
+  console.log(text);
+  $($(this).attr("href")).addClass("active").siblings().removeClass("active");
 });
 
 
