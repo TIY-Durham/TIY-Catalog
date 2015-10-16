a9a98b7a781c87e831318859694869ff9110a81b
Camille Mauban <camille.mauban@gmail.com>
2015-10-15 22:11:46 -0400

    favorite button turns red when clicked

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,14 @@
 
 // When you click on the collection button, menu drops down
-$('.collect-button').on('click', function(event){
+$('.collect-button').on('click', function(){
   $('.collect-drop-down').toggleClass('show');
+
+});
+
+
+
+// When you click on the favorite button, icon turns red
+$('.favorite-button').on('click', function(){
+  $(this).toggleClass('user-favorite');
 
 });
