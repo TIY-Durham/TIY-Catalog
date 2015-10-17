a76dc45ea8de5f7ea56b01e9cff2283a82f07cc6
David <dmwhite@gmail.com>
2015-10-17 08:11:52 -0400

    update main.js to be more specific to the the item-tabs

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,9 @@
 //controller for the item-tabs area of the Listing/Product page
 $("nav#item-tabs a").click(function(){
   $(this).addClass("active").siblings().removeClass("active");
-  $($(this).attr("href")).addClass("active").siblings().removeClass("active");
+  $("section.tabs").removeClass("active");
+  $($(this).attr("href")).addClass("active");
+  // .siblings().removeClass("active");
 });
 
 
