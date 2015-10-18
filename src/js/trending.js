81088b599b80055826e0ac4fbd32fb0ddb12bd11
Camille Mauban <camille.mauban@gmail.com>
2015-10-18 18:15:17 -0400

    service used to import image from json file

--- src/js/trending.js
+++ src/js/trending.js
@@ -5,45 +5,28 @@
   app.controller('TrendingController',[ '$http', '$scope', function($http, $scope){
        // TODO: Get this from JSON instead...
 
- // $http.get("../apis/etsy/listings/productpage/images.json")
- //  .then (function(data){
+ $http.get("../apis/etsy/listings/productpage/images.json")
+  .then (function(data){
 
     $scope.images = {
 
       "url_570xN": "https://img0.etsystatic.com/058/0/6292388/il_570xN.736798362_jvz2.jpg"
+    };
+
+
+    $scope.results = {
+      "title": "one-custom-engraved-wooden-guitar-pick",
+      "price": "8.50",
+      "shop_name": "PickslaysWoodworking"
 
     };
 
-   $scope.results = {
-"title": "one-custom-engraved-wooden-guitar-pick",
-"price": "8.50",
-"shop_name": "PickslaysWoodworking"
-  };
+    console.log(data); // logs images.json
 
-  // });
-
-
-  //
-  // $scope.results = {
-  //   title: 'lion mane',
-  //   price: 11.09,
-  //   shop: {
-  //     name: 'ShopName'
-  //     }
+  }); // END $HTTP
 
 
 
-
-
-
-
-
-
-  //     .then(function(data){
-  //
-  //     $scope.listing = data;
-  //
-  // });
 
 
 
