abc5af09619cc9860b1138e6fea90ea3d235be82
Camille Mauban <camille.mauban@gmail.com>
2015-10-18 19:28:04 -0400

    added 'add' button to drop down menu but still need to fix the width of the input box.

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,25 @@ $("nav#item-tabs a").click(function(){
   $("section.tabs").removeClass("active");
   $($(this).attr("href")).addClass("active");
 });
+
+
 // When you click on the collection button, menu drops down
 $('.collect-button').on('click', function(){
   $('.collect-drop-down').toggleClass('show');
-
+  $('.add-to-list').removeClass('active');
 });
+
 // When you click on the favorite button, icon turns red
 $('.favorite-button').on('click', function(){
   $(this).toggleClass('user-favorite');
-
 });
+
+$('input.list-text').on('click', function(){
+  $('.add-to-list').addClass('active');
+});
+
+
+
+
+
 })(); //end IIFE
