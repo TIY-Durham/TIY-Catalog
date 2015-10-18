5801aeed2e379df023d9d8c4b9b9b3635fa0f390
Kristine Erickson <ericksonk09@gmail.com>
2015-10-18 15:45:19 -0400

    setting up module

--- src/js/listing-header.js
+++ src/js/listing-header.js
@@ -1,3 +1,9 @@
 ;(function(){ // IIFE
+  var module = angular.module('listing-header', [ ]); // End module
 
+  module.run(['$rootScope', function($scope){
+    $scope. = {
+
+    };
+  }]);
 })(); // End IIFE
