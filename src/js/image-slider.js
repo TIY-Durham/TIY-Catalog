3d0d5ab63b4f55024dd82c2196428dec59ba4a05
Karlo Eleuterio <karloe@gmail.com>
2015-10-18 20:26:53 -0400

    images showing up using .run (removed .controller)

--- src/js/image-slider.js
+++ src/js/image-slider.js
@@ -6,21 +6,32 @@
     []
   ); // END module
 
-  // slider.run([ '$rootScope', function($s, $http){
-  //   $http.get("../../apis/etsy/listings/productpage/item100384851_images.json") // will work running html in Browser Sync
-  //     .then(function(response){
-  //       $s.item = response;
-  //     });
-  //   }]);
-
-  slider.controller('post-image', function($scope, $http){
+  slider.run([ '$rootScope','$http', function($scope, $http){
     $http.get("../../apis/etsy/listings/productpage/item100384851_images.json") // will work running html in Browser Sync
-    .then(function(response){
-      // slider.run([ '$rootScope', function(){
-      $scope.item = response;
-    // }]); //END run
-   }, function() {
-      console.log("cannot find data");
- }); // END then
- }); // END then
+      .success(function(data){
+        $scope.listingImages = data;
+      }, function() {
+        console.log("cannot find data");
+      });
+    }]);
+
+  // slider.controller('post-image', function($scope, $http){
+  //   $http.get("../../apis/etsy/listings/productpage/item100384851_images.json") // will work running html in Browser Sync
+  //   .then(function(response){
+  //     $scope.item = response;
+  //  }, function() {
+  //     console.log("cannot find data");
+  //     });
+  //   });
+
+  // var counter = 0;
+  // $('a#next').on('click', function () {
+  //   counter++;
+  //   $('img.img-active').value('clics', function () {
+  // })
+
+
+
+
+
 })(window);
