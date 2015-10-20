bb073ef6eb0cca7afdf84f5d1b889e59e33c8dc6
David Rogers <al.the.x@gmail.com>
2015-10-20 07:45:58 -0400

    Improve readability and follow in-class examples...
    * Corrected indentation of tags with `atom-beautify`
    * Changed module name back to `tiy-catalog--trending` as originally proposed
    * Replaced `TrendingController` with a `run` callback, as in class
    * Attached module to `main.controller` element instead of `html`, as in class

--- src/js/trending.js
+++ src/js/trending.js
@@ -1,36 +1,22 @@
 ;(function(){
 
-  var app = angular.module("trending", [ ]);
+  angular.module("tiy-catalog--trending", [ ])
+    .run(function($http, $scope){
 
-  app.controller('TrendingController',[ '$http', '$scope', function($http, $scope){
-       // TODO: Get this from JSON instead...
+      $http.get("/apis/etsy/listings/productpage/images.json")
+        .then(function(data){
+          $scope.images = {
+            "url_570xN": "https://img0.etsystatic.com/058/0/6292388/il_570xN.736798362_jvz2.jpg"
+          };
 
- $http.get("../apis/etsy/listings/productpage/images.json")
-  .then (function(data){
+          $scope.results = {
+            "title": "one-custom-engraved-wooden-guitar-pick",
+            "price": "8.50",
+            "shop_name": "PickslaysWoodworking"
+          };
 
-    $scope.images = {
-
-      "url_570xN": "https://img0.etsystatic.com/058/0/6292388/il_570xN.736798362_jvz2.jpg"
-    };
-
-
-    $scope.results = {
-      "title": "one-custom-engraved-wooden-guitar-pick",
-      "price": "8.50",
-      "shop_name": "PickslaysWoodworking"
-
-    };
-
-    console.log(data); // logs images.json data
-
-  }); // END $HTTP
-
-
-
-
-
-
-      // AND DON'T DELETE ANYTHING BELOW
-    } ])
+          console.log(data); // logs images.json data
+        }); // END $http.then
+      } ]) // END run
   ; // END module(tiy-catalog--trending)
 })(); // "Simple" IIFE
