7928af5126f7dbcb18786fae2a5b86971da4461a
David Rogers <al-the-x@users.noreply.github.com>
2015-10-21 11:13:19 -0400

    Revert "What happens when I open a PR for this?"

--- src/js/listing-header.js
+++
@@ -1,8 +0,0 @@
-;(function(){ // IIFE
-  var app = angular.module('listing-header', [ ]); // End module
-
-  app.run(['$rootScope', function($scope){
-    $scope.count = '249';
-    $scope.shop_name = 'PickslaysWoodworking';
-  }]);
-})(); // End IIFE
